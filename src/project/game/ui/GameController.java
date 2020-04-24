91cbee4d52be5f6b7b058ba64acb9f7a0956ec34
simo215d <simo215d@edu.easj.dk>
2020-04-24 13:24:14 +0200

    tilføjet et verdensbillede til spil baggrunden

--- src/project/game/ui/GameController.java
+++ src/project/game/ui/GameController.java
@@ -1,15 +1,26 @@
 package project.game.ui;
 
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import project.game.logic.Game;
 
 public class GameController {
     private Game game = new Game();
-    public void hello(Stage stage){
-        System.out.println("hi im the test in game!");
-        stage.setScene(new Scene(new Label("CORANA ALPHA 0.1"),800,600));
+    private ImageView worldImage;
+    private Scene scene;
+
+    public void newGame(Stage stage){
+        Image image = new Image(getClass().getResource("/project/resources/WorldMap.png").toString());
+        worldImage = new ImageView(image);
+        worldImage.setFitWidth(800);
+        worldImage.setFitHeight(600);
+        Group go = new Group(worldImage);
+        scene = new Scene(go,800,600);
+        stage.setScene(scene);
         game.start();
     }
 }
--- src/project/menu/MenuController.java
+++ src/project/menu/MenuController.java
@@ -3,14 +3,13 @@ package project.menu;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
 public class MenuController {
     public void newGameAction(ActionEvent event){
         Node source = (Node)event.getSource();
         Stage stage = (Stage) source.getScene().getWindow();
         System.out.println("new game starting up!");
-        Main.main.gameController.hello(stage);
+        Main.main.gameController.newGame(stage);
     }
 
     public void loadGameAction(ActionEvent event){
