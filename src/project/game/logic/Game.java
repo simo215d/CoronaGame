d6de4c5ef0132d05c7b04b2650c030b32057636c
simo215d <simo215d@edu.easj.dk>
2020-04-24 19:40:40 +0200

    added some TODOs

--- src/project/game/logic/Game.java
+++ src/project/game/logic/Game.java
@@ -3,6 +3,8 @@ package project.game.logic;
 import java.util.Observable;
 
 public class Game extends Observable {
+    //TODO MAKE 2d ARRAY WITH AREA CLASS WHICH ARE HAVE A CONTINENT VARIABLE AND A LIVE/DEAD/IMMUNE/INFECTED/CURED
+    //TODO SHOULD THE CONTROLLER LOGIC MAYBE BE IN HERE???
     public void start(){
         System.out.println("THE GAME HAS BEGUN!");
         setChanged();
