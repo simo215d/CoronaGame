9e1a93937f806b6a8cf14db628f3d5f744b080df
simo215d <simo215d@edu.easj.dk>
2020-04-24 12:46:02 +0200

    programmets quit knap virker nu. knapper er forbundet med controller

--- src/project/menu/MenuController.java
+++ src/project/menu/MenuController.java
@@ -1,4 +1,23 @@
 package project.menu;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
 public class MenuController {
+    public void newGameAction(ActionEvent event){
+        System.out.println("new game starting up!");
+    }
+
+    public void loadGameAction(ActionEvent event){
+        System.out.println("old game loading up!");
+    }
+
+    public void quitGameAction(ActionEvent event){
+        Node source = (Node)event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+        System.out.println("shutting down!");
+    }
 }
